1f375dbd68b8eb0b8f2f4721c65473b21a6fc4ab
Hoorad Ramezani <hooradramezani@gmail.com>
2019-10-06 12:08:00 +0330

    add images

--- Space Shooter/Space Shooter/GameViewController.swift
+++ Space Shooter/Space Shooter/GameViewController.swift
@@ -15,18 +15,18 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //        if let view = self.view as! SKView? {
-        //            // Load the SKScene from 'GameScene.sks'
-        //            if let scene = SKScene(fileNamed: "MainMenuScene") {
-        //                // Set the scale mode to scale to fit the window
-        //                scene.scaleMode = .aspectFill
-        //                // Present the scene
-        //                view.presentScene(scene)
-        //            }
-        //            view.ignoresSiblingOrder = true
-        //            view.showsFPS = true
-        //            view.showsNodeCount = true
-        //        }
+                if let view = self.view as! SKView? {
+                    // Load the SKScene from 'GameScene.sks'
+                    if let scene = SKScene(fileNamed: "MainMenuScene") {
+                        // Set the scale mode to scale to fit the window
+                        scene.scaleMode = .aspectFill
+                        // Present the scene
+                        view.presentScene(scene)
+                    }
+                    view.ignoresSiblingOrder = true
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
         
         let scene = MainMenuScene(size: CGSize(width: 1536, height: 2048))            // Configure the view.
         let skView = self.view as! SKView
